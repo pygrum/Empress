42c42cf6a5ed4524fe49c9161175c89ed4f98ebc
pygrum <pygrum.sec@gmail.com>
2023-11-26 21:49:54 +0000

    more robust build routine

--- service.py
+++ service.py
@@ -1,21 +1,73 @@
+import subprocess
+
 from monarch.builder import *
+from subprocess import STDOUT, check_output
 import os
 import base64
 
-def routine(req: BuildRequest) -> BuildResponse:
-    agent_id = req.params["id"]
-    cmd = "go build -o " + req.params["outfile"] + " -ldflags \"-X main.AgentID=" + agent_id + "\" ."
-    result = os.popen(cmd)
-    data = result.read()
-    status = result.close()
-    if status != None:
-	    status = 1
-    else:
-	    status = 0
-    with open(req.params["outfile"], "rb") as f:
-        data = f.read()
 
-    res = BuildResponse(status, "", base64.b64encode(data).decode('utf-8'))
+def routine(req: BuildRequest) -> BuildResponse:
+    out = req.params.get("outfile")
+    if out is None or out == "":
+        out = "empress"
+
+    out = str(os.path.basename(out))
+
+    agent_id = req.params.get("id")  # definitely exists
+    host = req.params.get("host")
+    port = req.params.get("port")
+    ci = req.params.get("callback_interval")
+    cs = req.params.get("callback_salt")
+    interval = req.params.get("interval")
+    salt = req.params.get("salt")
+    # if true, then debug is true. otherwise false
+    debug = str(req.params.get("debug")).lower() == "true"
+    mode = req.params.get("mode")
+
+    if mode == "session":
+        mode = 0
+    else:
+        mode = 1
+
+    with open("config/config.json", "w") as f:
+        j = {
+            "id": agent_id,
+            "host": host,
+            "port": port,
+            "callback_interval": int(ci),
+            "callback_salt": int(cs),
+            "interval": int(interval),
+            "salt": int(salt),
+            "mode": mode,
+        }
+        j_string = json.dumps(j)
+        f.write(j_string)
+
+    cmd = "go build -o %s " % out
+    if debug:
+        cmd += "-tags debug "
+    cmd += "."
+
+    data = b""
+    my_env = os.environ.copy()
+    my_env["GOOS"] = req.params.get("os")
+    my_env["GOARCH"] = req.params.get("arch")
+
+    try:
+        error = check_output(cmd.split(), stderr=STDOUT, env=my_env)
+    except subprocess.CalledProcessError as e:
+        error = e.output.decode('utf-8')
+
+    # path doesn't exist if build failed
+    if not os.path.exists(out):
+        status = 1
+    else:
+        status = 0
+        error = ""
+        with open(out, "rb") as f:
+            data = f.read()
+
+    res = BuildResponse(status, error, base64.b64encode(data).decode('utf-8'))
     return res
 
 
