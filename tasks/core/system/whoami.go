0926566c4e8e1f717aeebc7b8d86c4b49794cfc7
pygrum <pygrum.sec@gmail.com>
2023-12-01 20:30:27 +0000

    list groups in unix format

--- tasks/core/system/whoami.go
+++ tasks/core/system/whoami.go
@@ -1,6 +1,7 @@
 package system
 
 import (
+	"errors"
 	"github.com/pygrum/Empress/transport"
 	"os/user"
 	"strings"
@@ -14,7 +15,12 @@ func CmdWhoami(_ *transport.Request, response *transport.Response) {
 	}
 	//User.GroupIds now uses a Go native implementation when cgo is not available.
 	// https://tip.golang.org/doc/go1.18
-	id := "Name: " + u.Name + "\nUsername: " + u.Username + "\nUID: " + u.Uid + "\nGID: " + u.Gid + "\nGroups: "
+	uid := u.Uid + "(" + u.Username + ")"
+	gid, err := findGroup(u.Gid)
+	if err == nil {
+		gid = u.Gid + "(" + gid + ")"
+	}
+	id := "Name: " + u.Name + "\nUsername: " + u.Username + "\nUID: " + uid + "\nGID: " + gid + "\nGroups: "
 	groupString := ""
 	groups, err := u.GroupIds()
 	if err != nil {
@@ -22,7 +28,10 @@ func CmdWhoami(_ *transport.Request, response *transport.Response) {
 	} else {
 		var groupArray []string
 		for _, groupId := range groups {
-			g := findGroup(groupId)
+			g, err := findGroup(groupId)
+			if err == nil {
+				g = groupId + "(" + g + ")"
+			}
 			groupArray = append(groupArray, g)
 		}
 		groupString = strings.Join(groupArray, ", ")
@@ -35,10 +44,10 @@ func CmdWhoami(_ *transport.Request, response *transport.Response) {
 	})
 }
 
-func findGroup(gid string) string {
+func findGroup(gid string) (string, error) {
 	group, err := user.LookupGroupId(gid)
 	if err != nil {
-		return gid
+		return gid, errors.New("")
 	}
-	return group.Name
+	return group.Name, nil
 }
